0bedbb21120953f3a25d010f24d07dce2e25b7af
pucumt <weple@qq.com>
2018-08-01 14:03:11 +0800

    markdown show

--- models/mysql/question.js
+++ models/mysql/question.js
@@ -4,7 +4,7 @@ const db = require('../../db'),
 const Question = db.defineModel('questions', {
     title: db.STRING(50),
     author: db.STRING(1000), //作者信息
-    content: db.STRING,
+    content: db.TEXT,
     answer: db.STRING(1000),
     isChecked: { // only isChecked==1 will show in index, 0 is default 9 is refused
         type: db.INTEGER,
